90d9533f3674a399aaf8928caee0b90b8526390d
ola0x <rayjat51@gmail.com>
2023-08-23 01:43:47 +0100

    added the demo dashboard
    this web demo is the code i used in preview for the demo meetings.

---
+++ dashboard/auto_doc_p.py
@@ -0,0 +1,119 @@
+import os
+import keras
+import numpy as np
+import streamlit as st
+from util import process_img_path
+from keras import Model
+from keras import Sequential
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+from keras.layers import ZeroPadding2D, Convolution2D, MaxPooling2D, Dropout, Flatten, Activation
+
+# Build and Load model
+@st.cache_resource
+def load_model():
+    global model
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    model.add(Activation('softmax'))
+
+    model.load_weights('../docnet.h5')
+
+    return model
+
+def findCosineDistance(source_representation, test_representation):
+    a = np.matmul(np.transpose(source_representation), test_representation)
+    b = np.sum(np.multiply(source_representation, source_representation))
+    c = np.sum(np.multiply(test_representation, test_representation))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+ 
+def findEuclideanDistance(source_representation, test_representation):
+    euclidean_distance = source_representation - test_representation
+    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
+    euclidean_distance = np.sqrt(euclidean_distance)
+    return euclidean_distance
+
+@st.cache_data
+def process_folder(folder_path):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpeg") or filename.endswith(".png"):
+            image_path = os.path.join(folder_path, filename)
+            img = process_img_path(image_path)
+            images.append(img)
+    return np.vstack(images)
+
+def find_similar_images(image_representation, db_representations, threshold=0.06):
+    similarities = [findCosineDistance(image_representation, db_repr) for db_repr in db_representations]
+    similar_indices = [i for i, similarity in enumerate(similarities) if similarity < threshold]
+    return similar_indices
+
+if __name__ == "__main__":
+
+    db_paths = [['waec\\wa2.jpg', 'waec\\wa1.png']]  # List of image paths, each sublist contains image paths for a folder
+
+    model = load_model()
+    vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+    
+    db_representations = []
+    
+    for folder_images in db_paths:
+        folder_representations = []
+        for image_path in folder_images:
+            img_representation = vgg_face_descriptor.predict(process_img(image_path))[0, :]
+            folder_representations.append(img_representation)
+        db_representations.append(folder_representations)
+    
+    img1_representation = vgg_face_descriptor.predict(process_img('wat.jpg'))[0, :]
+    
+    similar_indices = []
+    
+    for folder_index, folder_representations in enumerate(db_representations):
+        similar_indices_folder = find_similar_images(img1_representation, folder_representations)
+        similar_indices.extend([(folder_index, idx) for idx in similar_indices_folder])
+    
+    if len(similar_indices) > 0:
+        print("The new image is verified")
+    else:
+        print("The new image is not verified")
---
+++ dashboard/demo.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import cv2
+import numpy as np 
+from auto_doc_p import load_model, Model, process_img_path, find_similar_images
+from util import process_img, load_images, process_img_path
+
+# init
+model = load_model()
+
+image_folder = "demo-f"
+
+if __name__ == "__main__":
+
+    html_temp = """
+        <div style="background-color:black;padding:5px">
+        <h2 style="color:white;text-align:center;">Demo AI Document verification</h2>
+        </div>
+        """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    image_files, manuscripts = load_images(image_folder)
+    view_manuscripts = st.multiselect("Select Document to verify", manuscripts)
+    n = st.number_input("Select Grid Width", 1, 5, 3)
+
+    view_images = []
+    for image_file in image_files:
+        if any(manuscript in image_file for manuscript in view_manuscripts):
+            view_images.append(image_file)
+    groups = []
+    for i in range(0, len(view_images), n):
+        groups.append(view_images[i:i+n])
+
+    for group in groups:
+        cols = st.columns(n)
+        for i, image_file in enumerate(group):
+            cols[i].image(image_file)
+    
+    db_paths = groups
+    vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+    
+    db_representations = []
+    
+    for folder_images in db_paths:
+        folder_representations = []
+        for image_path in folder_images:
+            img_representation = vgg_face_descriptor.predict(process_img_path(image_path))[0, :]
+            folder_representations.append(img_representation)
+        db_representations.append(folder_representations)
+
+    # print(groups)
+    image_upload = st.file_uploader(
+        "Upload Document Image", type=["png", "jpg", "jpeg"])
+    
+    if image_upload is not None:
+
+        # To See details
+        file_details = {"filename": image_upload.name, "filetype": image_upload.type,
+            "filesize": image_upload.size}
+
+            # Display the predict button just when an image is being uploaded
+        if not image_upload:
+            st.warning("Please upload an INVOICE image before proceeding!")
+            st.stop()
+        else:
+                # image_as_bytes = uploaded_file.read()
+            image_as_bytes = np.asarray(
+                bytearray(image_upload.read()), dtype=np.uint8)
+            img = cv2.imdecode(image_as_bytes, 1)
+            img_cv = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+            st.image(img_cv)
+            pred_button = st.button("Verify")
+        
+        if pred_button:
+            img1_representation = vgg_face_descriptor.predict(process_img(image_as_bytes))[0, :]
+            similar_indices = []
+    
+            for folder_index, folder_representations in enumerate(db_representations):
+                similar_indices_folder = find_similar_images(img1_representation, folder_representations)
+                similar_indices.extend([(folder_index, idx) for idx in similar_indices_folder])
+            
+            if len(similar_indices) > 0:
+                st.text("The new doc is verified")
+            else:
+                st.text("The doc is not verified")
